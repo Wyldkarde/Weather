db03be4e975ab1b7ccc1197fce28b3dc1f6d031d
Wyldkarde <Callendross@yahoo.com>
2016-04-10 22:55:46 -0400

    Inital

---
+++ Lab 11/WeatherMonth.java
@@ -0,0 +1,14 @@
+
+public class WeatherMonth
+{
+    int [] maxTemperature;
+    String monthName;
+    int daysInMonth;
+    public WeatherMonth()
+    {
+    }
+    public WeatherMonth(String monthName, int daysInMonth)
+    {
+    }
+    
+}
