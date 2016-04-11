9f7c96c51dd6b6f13854d5a6fea619fda58f8251
Wyldkarde <Callendross@yahoo.com>
2016-04-10 23:22:09 -0400

    Test 1 pass

--- Lab 11/WeatherMonth.java
+++ Lab 11/WeatherMonth.java
@@ -1,14 +1,62 @@
 
 public class WeatherMonth
 {
-    int [] maxTemperature;
-    String monthName;
-    int daysInMonth;
+    private int [] maxTemperature;
+    private String monthName;
+    private int daysInMonth;
     public WeatherMonth()
     {
     }
+
     public WeatherMonth(String monthName, int daysInMonth)
     {
     }
-    
+
+    public int [] getMaxTemperature()
+    {
+        return maxTemperature;
+    }
+
+    public void setDayMaxTemp(int dayOfMonth, int temperature)
+    {
+    }
+
+    public void displayWeatherMonth()
+    {
+    }
+
+    public void readMaxTempFile(String filename)
+    {
+    }
+
+    public String toString()
+    {
+        String nothing = "something";
+        return nothing;
+    }
+
+    public void setMaxTemperature(int [] maxTemperature)
+    {
+        this.maxTemperature = maxTemperature;
+    }
+
+    public String getMonthName()
+    {
+        return monthName;
+    }
+
+    public void setMonthName(String monthName)
+    {
+        this.monthName = monthName;
+    }
+
+    public int getDaysInMonth()
+    {
+        return daysInMonth;
+    }
+
+    public void setDaysInMonth(int daysInMonth)
+    {
+        this.daysInMonth = daysInMonth;
+    }
 }
